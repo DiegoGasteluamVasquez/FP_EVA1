5d7608ef3b7f717ebae440e3771725a3510a3c6d
DiegoGasteluamVasquez <L25550361@chihuahua2.tecnm.mx>
2025-08-27 22:05:57 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author DiegogvTEC ll
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARACION DE VARIABLES
+        //crearlas las variables para que se puedan usar
+        //Tipo de dato he identificador
+        String nombre;
+        byte edad;
+        //INICIACION
+        //USAMOS EL OPERADOR = -- OPERADOR DE ASIGNACION
+        //PON "DIEGO GASTELUM VASQUEZ" dentro de la variable nombre
+        nombre = "DIEGO GASTELUM VASQUEZ";
+        edad = 18;
+        //USO DE VARIABLES 
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
