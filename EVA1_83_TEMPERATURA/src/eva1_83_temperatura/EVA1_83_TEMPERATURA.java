1253102e599803b5f0f67fdad0822b90f0185590
DiegoGasteluamVasquez <L25550361@chihuahua2.tecnm.mx>
2025-09-09 17:02:37 -0600

    Add files via upload

---
+++ EVA1_83_TEMPERATURA/src/eva1_83_temperatura/EVA1_83_TEMPERATURA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_83_temperatura;
+
+/**
+ *
+ * @author DiegogvTEC ll
+ */
+public class EVA1_83_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double C,F;
+        C = 88;
+        F = ((9 * C / 5) + 32);
+        System.out.println("La escalaa de C a F es:");
+        System.out.println(F);
+    }
+    
+}
